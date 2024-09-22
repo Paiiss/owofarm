d829d423fc380f3a56e0e66e16efed436d7af27a
Paiss <pais.2k05@gmail.com>
2024-09-22 23:42:08 +0700

    Add config file with default values

---
+++ src/config/config.ts
@@ -0,0 +1,37 @@
+const config = {
+  "typing": true,
+  "prefix": "W",
+  "status": {
+    "hunt": true,
+    "battle": true,
+    "pray": true,
+    "curse": true,
+    "gamble": true
+  },
+  "interval": {
+    "animals": 1200000,
+    "pray": 303000,
+    "curse": 303500,
+    "gamble": {
+      "coinflip": 30000,
+      "slots": 30000
+    },
+    "hunt": {
+      "slowestTime": 16000,
+      "fastestTime": 32000
+    },
+    "battle": {
+      "slowestTime": 16000,
+      "fastestTime": 32000
+    }
+  },
+  "channels": {
+    "farm": "1287350084832333855",
+    "battle": "1287350142730637405",
+    "quest": "1287350170517770334",
+    "gamble": "1287350196258209812"
+  },
+  "owoId": "408785106942164992"
+}
+
+export default config;
