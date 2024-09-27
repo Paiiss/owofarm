d32498a4a5573a1441e462eb1ca16c2221a877b9
Paiss <pais.2k05@gmail.com>
2024-09-27 19:13:59 +0700

    Refactor config.ts to add and update properties

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,6 +1,7 @@
-import { Client, Message, TextChannel, Options } from 'discord.js-selfbot-v13'
+import { Client, Message, TextChannel, Options } from 'discord.js-selfbot-v13';
 import Logger from '../tools/logger';
 import config from '../config/config';
+import { superscriptToNumber } from '../tools/format';
 
 class AutoFarm {
   private token: string = '';
@@ -12,13 +13,14 @@ class AutoFarm {
     hunt: '',
     pray: '',
     curse: '',
-  }
+  };
   private timeoutId = {
     hunt: 0 as unknown as NodeJS.Timeout,
     battle: 0 as unknown as NodeJS.Timeout,
     pray: 0 as unknown as NodeJS.Timeout,
     curse: 0 as unknown as NodeJS.Timeout,
-  }
+  };
+  private inventory = {} as any;
   private checkList = {
     daily: false,
     vote: false,
@@ -26,11 +28,11 @@ class AutoFarm {
     quest: false,
     lootbox: false,
     crate: false,
-  }
+  };
   private total = {
     hunt_exp: 0,
     hunt: 0,
-  }
+  };
 
   constructor(token: string, setting: typeof config = config) {
     this.token = token;
@@ -41,7 +43,7 @@ class AutoFarm {
           interval: 10,
           lifetime: 15
         } */
-      }
+      },
     });
     this.logger = new Logger();
     this.setting = setting;
@@ -50,7 +52,7 @@ class AutoFarm {
       hunt: setting.channels.hunt,
       pray: setting.channels.quest || setting.channels.hunt,
       curse: setting.channels.quest || setting.channels.hunt,
-    }
+    };
   }
 
   start(): void {
@@ -59,87 +61,114 @@ class AutoFarm {
     this.client.on('ready', async () => {
       this.logger.setID(this.client.user?.username as string);
       this.logger.info('AutoFarm is ready!');
-      this.sendMessage(this.setting.channels.hunt, 'AutoFarm is ready!')
-    })
+      this.sendMessage(this.setting.channels.hunt, 'AutoFarm is ready!');
+    });
 
-    this.client.login(this.token).then(() => {
-      this.logger.info('Logged in');
-      this.sendCheckList()
-    }).catch((err) => {
-      if (err.code === 'TOKEN_INVALID') {
-        this.logger.danger('Invalid token');
-      } else {
-        this.logger.danger('An error occurred while logging in');
-      }
-    })
+    this.client
+      .login(this.token)
+      .then(() => {
+        this.logger.info('Logged in');
+        this.sendCheckList();
+      })
+      .catch((err) => {
+        if (err.code === 'TOKEN_INVALID') {
+          this.logger.danger('Invalid token');
+        } else {
+          this.logger.danger('An error occurred while logging in');
+        }
+      });
 
     this.client.on('messageCreate', async (message) => {
+      if (message.author.id !== this.setting.owoId) return;
       // OwO Captcha Handler
-      if (message.author.id === this.setting.owoId && message.content.match(/human|captcha|dm|banned|https:\/\/owobot.com\/captcha/g) && (message.content.match(new RegExp(`${this.client.user?.id}`, 'g')) || message.mentions.users.first()?.id === this.client.user?.id)) return this.handleOwoCaptcha(), console.log(message)
+      if (
+        message.content.match(/human|captcha|dm|banned|https:\/\/owobot.com\/captcha/g) &&
+        (message.content.match(new RegExp(`${this.client.user?.id}`, 'g')) ||
+          message.mentions.users.first()?.id === this.client.user?.id)
+      )
+        return this.handleOwoCaptcha(), console.log(message);
 
-      // OwO Verification Handler 
-      if (message.author.id === this.setting.owoId && message.channel.type === 'DM' && message.content.match(/👍|verified|Thank you! :3/g)) this.handleOwoSuccessVerification(message.content)
+      // OwO Verification Handler
+      if (message.channel.type === 'DM' && message.content.match(/👍|verified|Thank you! :3/g))
+        this.handleOwoSuccessVerification(message.content);
 
       // CheckList Handler
-      if (message.channelId === this.setting.channels.hunt && message.author.id === this.setting.owoId && message.embeds[0]?.description && message.embeds[0]?.author?.name?.match(new RegExp(`${message.guild?.members.me?.nickname}'s Checklist`, 'g'))) this.handleCheckList(message.embeds[0].description)
+      if (
+        message.channelId === this.setting.channels.hunt &&
+        message.embeds[0]?.description &&
+        message.embeds[0]?.author?.name?.match(new RegExp(`${message.guild?.members.me?.nickname}'s Checklist`, 'g'))
+      )
+        this.handleCheckList(message.embeds[0].description);
 
-      // Check Hunt Gems 
-      if (message.channelId === this.setting.channels.hunt && message.author.id === this.setting.owoId && message.content?.match(new RegExp(`${message.guild?.members.me?.nickname}\\*\\*, hunt`, 'g'))) this.handleHuntGems(message.content)
-    })
+      // Check Hunt Gems
+      if (message.content?.match(new RegExp(`${message.guild?.members.me?.nickname}\\*\\*, hunt`, 'g')))
+        this.handleHuntGems(message.content);
+
+      // Inventory Handler
+      if (message.content?.match(new RegExp(`${message.guild?.members.me?.nickname}'s Inventory`, 'g')))
+        this.handleInventory(message.content);
+    });
   }
 
   handleOwoCaptcha() {
-    this.botStatus = false
-    this.logger.info('OwO captcha detected')
-    this.stopAutoFarm()
+    this.botStatus = false;
+    this.logger.info('OwO captcha detected');
+    this.stopAutoFarm();
   }
 
   handleOwoSuccessVerification(message: string): void {
-    this.logger.info('OwO verification success')
-    this.botStatus = true
-    this.startAutoFarm()
+    this.logger.info('OwO verification success');
+    this.botStatus = true;
+    this.startAutoFarm();
   }
 
   private handleCheckList(message: string): void {
-    this.logger.info('Checking checklist 📜')
+    this.logger.info('Checking checklist 📜');
     if (message.match(/⬛ 🎁/g)) {
-      this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['daily']))
-      this.checkList.daily = true
+      this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['daily']));
+      this.checkList.daily = true;
     } else {
-      this.checkList.daily = true
+      this.checkList.daily = true;
     }
-    this.checkList.vote = message.match(/⬛ 📝/g) ? false : true
-    this.checkList.cookie = message.match(/⬛ 🍪/g) ? false : true
-    this.checkList.quest = message.match(/⬛ 📜/g) ? false : true
-    this.checkList.lootbox = message.match(/⬛ 💎/g) ? false : true
-    this.checkList.crate = message.match(/⬛ ⚔/g) ? false : true
+    this.checkList.vote = message.match(/⬛ 📝/g) ? false : true;
+    this.checkList.cookie = message.match(/⬛ 🍪/g) ? false : true;
+    this.checkList.quest = message.match(/⬛ 📜/g) ? false : true;
+    this.checkList.lootbox = message.match(/⬛ 💎/g) ? false : true;
+    this.checkList.crate = message.match(/⬛ ⚔/g) ? false : true;
 
-    let checkListMessage: string[] = []
+    let checkListMessage: string[] = [];
     for (const key in this.checkList) {
-      const value = this.checkList[key as keyof typeof this.checkList]
-      checkListMessage.push(`${value ? '✅' : '❌'} ${key}`)
+      const value = this.checkList[key as keyof typeof this.checkList];
+      checkListMessage.push(`${value ? '✅' : '❌'} ${key}`);
     }
 
-    this.logger.info(`Checklist: ${checkListMessage.join(' | ')}`)
+    this.logger.info(`Checklist: ${checkListMessage.join(' | ')}`);
 
-    if (this.checkList.daily && this.checkList.cookie && this.checkList.quest && this.checkList.lootbox && this.checkList.crate) {
-      this.logger.info('All checklist completed ✅')
-      if (this.setting.checklist_completed) this.stopAutoFarm()
+    if (
+      this.checkList.daily &&
+      this.checkList.cookie &&
+      this.checkList.quest &&
+      this.checkList.lootbox &&
+      this.checkList.crate
+    ) {
+      this.logger.info('All checklist completed ✅');
+      if (this.setting.checklist_completed) this.stopAutoFarm();
     }
 
-    // this.startAutoFarm();
+    this.startAutoFarm();
+    this.checkInventory();
   }
 
   private handleHuntGems(message: string): void {
-    this.total.hunt += 1
-    const gems = []
+    this.total.hunt += 1;
+    const gems = [];
 
-    if (!message.includes('gem1')) gems.push('gem1')
-    if (!message.includes('gem3')) gems.push('gem3')
-    if (!message.includes('gem4')) gems.push('gem4')
+    if (!message.includes('gem1')) gems.push('gem1');
+    if (!message.includes('gem3')) gems.push('gem3');
+    if (!message.includes('gem4')) gems.push('gem4');
 
     if (gems.length > 0) {
-      this.logger.info(`Missing gems: ${gems.join(', ')}`)
+      this.logger.info(`Missing gems: ${gems.join(', ')}`);
     }
 
     let match;
@@ -149,78 +178,132 @@ class AutoFarm {
       this.total.hunt_exp += xp;
     }
 
-    this.logger.info(`Total XP from hunting: ${this.total.hunt_exp}`)
+    this.logger.info(`Total XP from hunting: ${this.total.hunt_exp}`);
+  }
+
+  private handleInventory(message: string): void {
+    const regex = /`(\d+|2--)`<:\w+:\d+>([⁰¹²³⁴⁵⁶⁷⁸⁹]+)/g;
+    let match;
+    const result = {} as { [key: string]: number };
+
+    while ((match = regex.exec(message)) !== null) {
+      const quantity: string = match[1];
+      const itemCount: number = superscriptToNumber(match[2]);
+
+      result[quantity] = itemCount;
+    }
+    this.inventory = result;
+
+    if ('100' in this.inventory && this.setting.status.crate) this.openCrate;
+    if ('050' in this.inventory && this.setting.status.lootbox) this.openLootbox;
+    if ('049' in this.inventory && this.setting.status.lootbox_fabled) this.openLootboxfabled;
   }
 
   async sendMessage(channel: string, message: string): Promise<void> {
-    if (!this.botStatus) return this.logger.danger('Bot is not ready')
-    const channelToSend = this.client.channels.cache.get(channel) as TextChannel
-    if (!channelToSend) this.logger.danger('Channel not found')
-    if (this.setting.typing) await channelToSend.sendTyping()
+    if (!this.botStatus) return this.logger.danger('Bot is not ready');
+    const channelToSend = this.client.channels.cache.get(channel) as TextChannel;
+    if (!channelToSend) this.logger.danger('Channel not found');
+    if (this.setting.typing) await channelToSend.sendTyping();
     channelToSend?.send(message).catch((err) => {
       this.logger.danger(`An error occurred while sending a message: ${err}`);
-    })
+    });
   }
 
   randomPrefix(message: string[]): string {
-    return ['owo', this.setting.prefix || 'owo'][Math.floor(Math.random() * 2)] + ' ' + message[Math.floor(Math.random() * message.length)]
+    return (
+      ['owo', this.setting.prefix || 'owo'][Math.floor(Math.random() * 2)] +
+      ' ' +
+      message[Math.floor(Math.random() * message.length)]
+    );
   }
 
   async startAutoFarm(): Promise<void> {
-    if (!this.botStatus) return this.logger.danger('Bot is not ready')
-    if (this.setting.status.hunt) this.autoHunt()
-    if (this.setting.status.battle) this.autoBattle()
-    if (this.setting.status.pray) this.autoPray()
-    if (this.setting.status.curse) this.autoCurse()
+    if (!this.botStatus) return this.logger.danger('Bot is not ready');
+    if (this.setting.status.hunt) this.autoHunt();
+    if (this.setting.status.battle) this.autoBattle();
+    if (this.setting.status.pray) this.autoPray();
+    if (this.setting.status.curse) this.autoCurse();
   }
 
   private async sendCheckList(): Promise<void> {
-    this.logger.info('Sending checklist 📜')
-    this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['cl', 'checklist']))
+    this.logger.info('Sending checklist 📜');
+    this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['cl', 'checklist']));
   }
 
   private async autoHunt(): Promise<void> {
-    this.timeoutId.hunt = setTimeout(async () => {
-      this.logger.info('Hunting')
-      await this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['hunt', 'h']))
-      this.autoHunt()
-    }, Math.floor(Math.random() * (this.setting.interval.hunt.fastestTime - this.setting.interval.hunt.slowestTime + 1) + this.setting.interval.hunt.slowestTime))
+    this.timeoutId.hunt = setTimeout(
+      async () => {
+        this.logger.info('Hunting');
+        await this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['hunt', 'h']));
+        this.autoHunt();
+      },
+      Math.floor(
+        Math.random() * (this.setting.interval.hunt.fastestTime - this.setting.interval.hunt.slowestTime + 1) +
+          this.setting.interval.hunt.slowestTime
+      )
+    );
   }
 
   private async autoBattle(): Promise<void> {
-    this.timeoutId.battle = setTimeout(async () => {
-      this.logger.info('Battling')
-      await this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['battle', 'b']))
-      this.autoBattle()
-    }, Math.floor(Math.random() * (this.setting.interval.battle.fastestTime - this.setting.interval.battle.slowestTime + 1) + this.setting.interval.battle.slowestTime))
+    this.timeoutId.battle = setTimeout(
+      async () => {
+        this.logger.info('Battling');
+        await this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['battle', 'b']));
+        this.autoBattle();
+      },
+      Math.floor(
+        Math.random() * (this.setting.interval.battle.fastestTime - this.setting.interval.battle.slowestTime + 1) +
+          this.setting.interval.battle.slowestTime
+      )
+    );
   }
 
   private async autoPray(): Promise<void> {
-    this.logger.info('Praying')
-    await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['pray']))
+    this.logger.info('Praying');
+    await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['pray']));
 
     this.timeoutId.pray = setTimeout(async () => {
-      this.logger.info('Praying')
-      await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['pray']))
-      this.autoPray()
-    }, this.setting.interval.pray)
+      this.logger.info('Praying');
+      await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['pray']));
+      this.autoPray();
+    }, this.setting.interval.pray);
   }
 
   private async autoCurse(): Promise<void> {
-    this.logger.info('Cursing')
-    await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['curse']))
+    this.logger.info('Cursing');
+    await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['curse']));
 
     this.timeoutId.curse = setTimeout(async () => {
-      this.logger.info('Cursing')
-      await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['curse']))
-      this.autoCurse()
-    }, this.setting.interval.curse)
+      this.logger.info('Cursing');
+      await this.sendMessage(this.setting.channels.quest, this.randomPrefix(['curse']));
+      this.autoCurse();
+    }, this.setting.interval.curse);
+  }
+
+  private checkInventory(): void {
+    this.logger.info('Checking inventory 🧾');
+    this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['inv', 'inventory']));
+  }
+
+  private openLootbox(): void {
+    this.logger.info('Opening lootbox 🎁');
+    this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['lootbox', 'lb']) + ' all');
+  }
+
+  private openLootboxfabled(): void {
+    this.logger.info('Opening lootbox fabled 🎁');
+    this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['lootbox', 'lb']) + ' fabled all');
+  }
+
+  private openCrate(): void {
+    this.logger.info('Opening crate 📦');
+    this.sendMessage(this.setting.channels.hunt, this.randomPrefix(['crate']) + ' all');
   }
 
   stopAutoFarm(): void {
     for (const id in this.timeoutId) {
       const key = id as keyof typeof this.timeoutId;
-      if (this.timeoutId[key]) clearTimeout(this.timeoutId[key])
+      if (this.timeoutId[key]) clearTimeout(this.timeoutId[key]);
     }
   }
 }
