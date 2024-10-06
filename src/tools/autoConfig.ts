e5611a54294e296bb792851e770107fa859e3cdd
Paiss <pais.2k05@gmail.com>
2024-10-06 19:40:48 +0700

    Refactor autoConfig.ts to add config file watcher

--- src/tools/autoConfig.ts
+++ src/tools/autoConfig.ts
@@ -2,34 +2,50 @@ import fs from 'fs';
 import path from 'path';
 import config from '../config/config';
 
-const checkConfig = (name: string): typeof config => {
+const checkAndWatchConfig = (name: string, callback: (config: any) => void): void => {
   const pathToConfig = path.join(__dirname, '..', '..', 'config', `${name}.json`);
+
   const exists = fs.existsSync(pathToConfig);
   if (!exists) {
     fs.writeFileSync(pathToConfig, JSON.stringify(config, null, 2));
-
-    return config as typeof config;
+    callback(config);
   } else {
     const currentConfig = JSON.parse(fs.readFileSync(pathToConfig, 'utf-8'));
+    let isChanged = false;
     for (const key in config) {
-      if (!currentConfig[key]) {
+      if (!(key in currentConfig)) {
         currentConfig[key] = config[key as keyof typeof config];
+        isChanged = true;
       }
 
       if (typeof config[key as keyof typeof config] === 'object') {
         const subConfig = config[key as keyof typeof config] as any;
         const currentSubConfig = currentConfig[key];
         for (const subKey in subConfig) {
-          if (!currentSubConfig[subKey]) {
+          if (!(subKey in currentConfig[key])) {
             currentSubConfig[subKey] = subConfig[subKey];
+            isChanged = true;
           }
         }
       }
     }
-    fs.writeFileSync(pathToConfig, JSON.stringify(currentConfig, null, 2));
 
-    return currentConfig as typeof config;
+    if (isChanged) {
+      fs.writeFileSync(pathToConfig, JSON.stringify(currentConfig, null, 2));
+    }
+    callback(currentConfig);
+
+    fs.watch(pathToConfig, (eventType, filename) => {
+      if (eventType === 'change') {
+        try {
+          const currentConfig = JSON.parse(fs.readFileSync(pathToConfig, 'utf-8'));
+          callback(currentConfig);
+        } catch (error: any) {
+          console.error(`Error reading or parsing config file: ${error.message}`);
+        }
+      }
+    });
   }
 };
 
-export default checkConfig;
+export default checkAndWatchConfig;
